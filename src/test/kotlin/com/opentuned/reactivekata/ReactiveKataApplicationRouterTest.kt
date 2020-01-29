5e3edc8e9831331588949d36df6fed0a4d4a326b
Jeroen Nouws <34285209+MOES-Media@users.noreply.github.com>
2020-01-29 15:43:09 +0100

    Update ReactiveKataApplicationRouterTest.kt

--- src/test/kotlin/com/opentuned/reactivekata/ReactiveKataApplicationRouterTest.kt
+++ src/test/kotlin/com/opentuned/reactivekata/ReactiveKataApplicationRouterTest.kt
@@ -1,30 +1,42 @@
 package com.opentuned.reactivekata
 
+import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.test.web.reactive.server.WebTestClient
+import org.springframework.web.reactive.function.server.RouterFunction
+import org.springframework.web.reactive.function.server.ServerResponse
 
 @SpringBootTest
 class ReactiveKataApplicationRouterTest {
 
-	val webTestClient = WebTestClient.bindToRouterFunction(routerFunction()).build()
+    @Test
+    fun contextLoads() {
+    }
 
-	@Test
-	fun contextLoads() {
-	}
+    @Autowired
+    private lateinit var routerFunction: RouterFunction<ServerResponse>
 
-	@Test
-	fun `calling hello world endpoint returns 200`() {
-		webTestClient.get().uri("/frp/hello-world")
-				.exchange()
-				.expectStatus().isOk
-	}
+    private lateinit var webTestClient: WebTestClient;
 
-	@Test
-	fun `calling hello world endpoint with query param returns param in body and 200`() {
-		webTestClient.get().uri("/frp/hello-world?name=foo")
-				.exchange()
-				.expectStatus().isOk
-				.expectBody().returnResult().responseBody.toString().contains("Hello foo")
-	}
+    @BeforeEach
+    fun setupWebClient() {
+        webTestClient = WebTestClient.bindToRouterFunction(routerFunction).build()
+    }
+
+    @Test
+    fun `calling hello world endpoint returns 200`() {
+        webTestClient.get().uri("/frp/hello-world")
+                .exchange()
+                .expectStatus().isOk
+    }
+
+    @Test
+    fun `calling hello world endpoint with query param returns param in body and 200`() {
+        webTestClient.get().uri("/frp/hello-world?name=foo")
+                .exchange()
+                .expectStatus().isOk
+                .expectBody().returnResult().responseBody.toString().contains("Hello foo")
+    }
 }
