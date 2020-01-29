8e498901662467a39cca8220765e35ff2b141e3e
Jeroen Nouws <34285209+MOES-Media@users.noreply.github.com>
2020-01-29 15:42:17 +0100

    Update ReactiveKataApplication.kt

--- src/main/kotlin/com/opentuned/reactivekata/ReactiveKataApplication.kt
+++ src/main/kotlin/com/opentuned/reactivekata/ReactiveKataApplication.kt
@@ -1,8 +1,9 @@
 package com.opentuned.reactivekata
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
-import org.springframework.context.support.beans
+import org.springframework.boot.runApplication
+import org.springframework.context.annotation.Bean
+import org.springframework.context.annotation.Configuration
 import org.springframework.web.reactive.function.server.RouterFunction
 import org.springframework.web.reactive.function.server.ServerResponse
 import org.springframework.web.reactive.function.server.router
@@ -13,27 +14,16 @@ import reactor.core.publisher.Mono
 class ReactiveKataApplication
 
 fun main(args: Array<String>) {
-	//runApplication<ReactiveKataApplication>(*args)
-	SpringApplicationBuilder(ReactiveKataApplication::class.java)
-			.initializers(
-				beans {
-					bean {
-						router {
-							GET("/frp/hello") {
-								ServerResponse.ok().body(Mono.just("Hello"), String::class.java)
-							}
-						}.andOther(routerFunction())
-					}
-			})
-			.build()
-			.run(*args)
+    runApplication<ReactiveKataApplication>(*args)
 }
 
-fun routerFunction(): RouterFunction<ServerResponse> {
-	return router {
-		GET("/frp/hello-world") {
-			ServerResponse.ok()
-					.body(Mono.just("Hello ${it.queryParam("name").ifPresent { it }}"), String::class.java)
-		}
-	}
+@Configuration
+class RouterConfig {
+    @Bean
+    fun routerFunction(): RouterFunction<ServerResponse> = router {
+        GET("/frp/hello-world") {
+            ServerResponse.ok()
+                    .body(Mono.just("Hello ${it.queryParam("name").orElse("")}"), String::class.java)
+        }
+    }
 }
